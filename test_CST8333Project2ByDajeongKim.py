01a09314393ab3bb609e048954409657f438b251
kim00430 <kim00430@algonquinlive.com>
2021-10-17 06:42:08 -0400

    Finished assignment2 presentation

--- test_CST8333Project2ByDajeongKim.py
+++ test_CST8333Project2ByDajeongKim.py
@@ -1,9 +1,15 @@
 from unittest import TestCase
-"""
-* CST8333 Assignment2 UnitTest
-* Dajeong Kim (#040983172)
-"""
+
 
 class TestOptions(TestCase):
     def test_display_f(self):
+        self.fail()
+
+    def test_add_f(self):
+        self.fail()
+
+    def test_edit_f(self):
+        self.fail()
+
+    def test_delete_f(self):
         self.fail()
